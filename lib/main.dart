4dcf1e8e573f9b1ceb9d05fc1252df95315180b1
camh <camh3190@gmail.com>
2020-03-13 20:42:57 -0600

    first commit, value_objects, video 2 del tutoria

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My New App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
+        fontFamily: 'Roboto',
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('New App'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello Camh'),
+          ),
+        ),
+      );
+  }
+}
