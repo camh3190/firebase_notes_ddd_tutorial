7c3c90cd9ed0b0acfe248876b47fee776a0275a6
camh <camh3190@gmail.com>
2020-03-23 12:43:02 -0600

    clase 4, no le di salvar a uno de los archivos

--- lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/bloc/sign_in_form_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebasenotesddd/domain/auth/auth_failure.dart';
+import 'package:firebasenotesddd/domain/auth/i_auth_facade.dart';
 import 'package:firebasenotesddd/domain/auth/value_objects.dart';
 import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
@@ -14,6 +15,10 @@ part 'sign_in_form_state.dart';
 part 'sign_in_form_bloc.freezed.dart';
 
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade);
+  
   @override
   SignInFormState get initialState => SignInFormState.initial();
 
