ec5a013e90d6a5b291ea9f78b0e26e64d94b9ea6
camh <camh3190@gmail.com>
2020-05-16 17:50:07 -0600

    clase 8 sign-in form UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,10 @@
 import 'package:firebasenotesddd/injection.dart';
+import 'package:firebasenotesddd/presentation/core/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
 void main() {
   configureInjection(Environment.prod);
-  runApp(MyApp());
+  runApp(AppWidget());
   }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My New App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: const Color(0xFF2196f3),
-        accentColor: const Color(0xFF2196f3),
-        canvasColor: const Color(0xFFfafafa),
-        fontFamily: 'Roboto',
-      ),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('New App'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello Camh'),
-          ),
-        ),
-      );
-  }
-}
